c05098550986b8029d88c3ff4311ac811c467d7a
沉寂 <bianzhifu@gmail.com>
2021-12-03 19:40:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,12 +28,12 @@ install() {
   chmod 777 /opt/ServerStatus/
 
   echo -e "下载ServerStatus"
-  wget -O ServerStatus_liunx_${os_arch} https://github.com/bianzhifu/ServerStatus/releases/download/v1.0.0/ServerStatus_liunx_${os_arch} >/dev/null 2>&1
+  wget -O ServerStatus_liunx_${os_arch} https://github.com/bianzhifu/ServerStatus/releases/download/v1.0.0/ServerStatus_linux_${os_arch} >/dev/null 2>&1
   if [[ $? != 0 ]]; then
-    echo -e "${red}下载失败,https://github.com/bianzhifu/ServerStatus/releases/download/v1.0.0/ServerStatus_liunx_${os_arch}"
+    echo -e "${red}下载失败,https://github.com/bianzhifu/ServerStatus/releases/download/v1.0.0/ServerStatus_linux_${os_arch}"
     return 0
   fi
-  mv ServerStatus_liunx_${os_arch} /opt/ServerStatus/ServerStatus
+  mv ServerStatus_linux_${os_arch} /opt/ServerStatus/ServerStatus
   chmod +x /opt/ServerStatus/ServerStatus
 
   echo -e "> 修改配置"
